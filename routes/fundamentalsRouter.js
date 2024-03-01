5cf84bad11d679e0855bbcb229cb1931df9cc486
Mini Lemon <quyto111@gmail.com>
2024-03-01 11:47:29 +0700

    temp complete

--- routes/fundamentalsRouter.js
+++ routes/fundamentalsRouter.js
@@ -24,7 +24,33 @@ router.get('/Check-if-a-number-is-a-whole-number', function(req, res, next) {
 router.get('/Multiplication-division-and-comparison-operators', function(req, res, next) {
   let a = req.query.a;
   let b = req.query.b;
-  res.send({result: a<b?a/b:a*b});
+  res.send({
+    variableData: variableData,
+    result: Multiplication_division_and_comparison_operators(a,b)
+  });
 });
+function Multiplication_division_and_comparison_operators(a,b){
+  return a<b?a/b:a*b;
+}
+router.get('/Round-a-number-to-2-decimal-places', function(req, res, next) {
+  let a = req.query.a;
+  res.send({
+    variableData: variableData,
+    result: Round_a_number_to_2_decimal_places(a)
+  });
+});
+function Round_a_number_to_2_decimal_places(a){
+  return Math.round(a*100)/100;
+}
+router.get('/Split_a_number_into_its_digits', function(req, res, next) {
+  let a = req.query.a;
+  res.send({
+    variableData: variableData,
+    result: Split_a_number_into_its_digits(a)
+  });
+});
+function Split_a_number_into_its_digits(a){
+  return (""+a).split('').map(e=>parseInt(e));
+}
 
 module.exports = router;
